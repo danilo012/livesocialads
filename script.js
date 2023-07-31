522b176e6d8c6beb2d245b84e1db116da74b8a4d
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-07-31 12:57:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -773,3 +773,18 @@ class PostContent extends React.Component {
 
 ReactDOM.render( /*#__PURE__*/React.createElement(Application, null), document.getElementById('root'));
 
+import React, { useEffect, useState } from "react";
+
+const useLocalStorage = (key, defaultValue) =&gt; {
+  const [value, setValue] = useState(
+    JSON.parse(localStorage.getItem(key, defaultValue))
+  );
+
+  useEffect(() =&gt; {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;

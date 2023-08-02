f2c333702e904ea7847831815ed02f26ece494a2
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-08-02 17:35:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -763,6 +763,13 @@ class PostContent extends React.Component {
   }}
 
 
+
+
+ReactDOM.render( /*#__PURE__*/React.createElement(Application, null), document.getElementById('root'));
+
+
+
+
 function open_about() {
     remove_section();
     remove_nav();
@@ -770,5 +777,3 @@ function open_about() {
     add.innerHTML = '<center><button class="back-button" onclick="back_home()">Back</button><h1 id="logo"><a href="./index.html">GOLDEN COINS </a></h1><br><br><iframe src="./README.md" width="100%" height="900" style="background-color: #b46b59;"></iframe></center>';
   window.open(link);
     open_about();
-
-ReactDOM.render( /*#__PURE__*/React.createElement(Application, null), document.getElementById('root'));
